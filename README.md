60e453243f8a111aa2c513b6014304aa0b2a41f8
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-12-17 17:40:42 +0100

    v2 of WebP converter :sparkles:

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 I had two reasons for writing this piece of code:
 
 - Google's `cwebp` command only lets you compress and convert one file at a time, which is often very tedious with a larger set of files.
-- I wanted to learn shell, and what better way to do it than by solving a problem I have just created 🙌🏼
+- I could've done this very easily in Python, but where's the fun in that? I wanted to learn shell, and what better way to do it than by solving a problem I have just created 🙌🏼
 
-You need the `webp` formula installed on your machine - the program will check whether it's installed or not, and will install it if necessary. It will also install [Homebrew](http://brew.sh) for you if you don't have it installed 🍺
+You need the `webp` formula installed through Homebrew to use this script. Alternatively, you can download and run the `cwebp` excutable, which this program can automatically download and configure for you, should you desire. It can also be manually downloaded for your OS from [here](https://storage.googleapis.com/downloads.webmproject.org/releases/webp/index.html).
 
 The images are being compressed with a hardcoded factor of 80 - meaning the output file will be 80% its original quality. This can be changed by modyfying the value after the `-q` option in the `cwebp` command. For all available options, visit [Google's docs](https://developers.google.com/speed/webp/docs/cwebp)!
 
@@ -19,11 +19,17 @@ First, make the file executable by running
 $ chmod +x convert.sh
 ```
 
-You will be asked for a directory in which your files are located - this can be either relative or absolute!
+Then, run the scrip in current working directory:
+
+```bash
+~WebP-Batch-Converter $ ./convert.sh
+```
+
+After going through the installation process (should you not have `webp` installed on your machine already), you will be asked to provide an input directory with all your image files.
 
 ```bash
 # This will take folder named "files" located in the same working directory
-$ ./convert.sh ./files
+"Where are your images located? Provide folder:" > ./files
 ```
 
 Converted WebP files will be put in the script's working directory
--- convert.sh
+++ convert.sh
@@ -2,25 +2,60 @@
 # and convert them to WebP format using google's cwebp lib.
 # For now compression rate is hardcoded to 80..
 
-# Check if homebrew is installed
-which -s brew
-if [[ $? != 0 ]] ; then
-    # Install it if not
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-    # Check if webp is installed
-    # brew -ls output is rather slow so need to wait a bit here
-    if brew ls --versions webp sleep 2 > /dev/null; then
-        # if not - install it
-        brew install webp
-    fi
-    read -p 'Where are your images located? Provide folder: ' dir
-    for file in "$dir"/*; do
-        ## strip the file name of the path and extension
-        ext=${file##*.}
-        fname=`basename $file .$ext`
-        cwebp -q 80 $file -o $fname".webp"
-        # Example cwebp input:
-        # cwebp -q 50 file.png -o file.webp
-    done
-fi
+read -n1 -p "Welcome. This program requires the webp package to be installed. Do you have it installed? [Y/N] " an1
+case "$an1" in
+    y|Y)
+        echo " "
+        read -p "Where are your images located? Provide folder: " dir
+        for file in "$dir"/*; do
+            ## strip the file name of the path and extension
+            ext=${file##*.}
+            fname=`basename $file .$ext`
+            cwebp -q 80 $file -o $fname".webp"
+            # Example cwebp input:
+            # cwebp -q 50 file.png -o file.webp
+        done
+        ;;
+    n|N)
+        echo " "
+        read -n1 -p "Would you like me to download it for you? [Y/N] " an2
+        case "$an2" in
+            y|Y)
+                # Download the appropriate file based on user's OS
+                if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+                    curl https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.1.0-linux-x86-64.tar.gz -o libwebp-1.1.0-linux-x86-64.tar.gz
+                    tar -xvzf libwebp-1.1.0-linux-x86-64.tar.gz
+                elif [[ "$OSTYPE" == "darwin"* ]]; then
+                    curl https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.1.0-mac-10.15.tar.gz -o libwebp-1.1.0-mac-10.15.tar.gz
+                    tar -xvzf libwebp-1.1.0-mac-10.15.tar.gz    
+                fi
+                # After unzipping, run the script
+                echo "That's done! All the necessary files have been downloaded and unzipped!\n"
+                read -p "Where are your images located? Provide folder: " dir
+                for file in "$dir"/*; do
+                    # Strip the file name of the path and extension
+                    ext=${file##*.}
+                    fname=`basename $file .$ext`
+                    # Based on OS, run the appropriate file
+                    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+                        ./libwebp-1.1.0-linux-x86-64/bin/cwebp -q 80 $file -o $fname".webp"
+                    elif [[ "$OSTYPE" == "darwin"* ]]; then
+                        ./libwebp-1.1.0-mac-10.15/bin/cwebp -q 80 $file -o $fname".webp"
+                    fi
+                done
+                ;;
+            n|N)
+                echo "Please install the webp package before continuing!"
+                exit
+                ;;
+            *)
+                echo "You have provided incorrect input"
+                ;;
+        esac
+        ;;
+    *)
+        echo "You have provided incorrect input"
+        ;;  
+esac
+
+
