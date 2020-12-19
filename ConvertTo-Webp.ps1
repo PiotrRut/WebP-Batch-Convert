d9c14b3677369056fb1f05e1b692c615b6f54822
Reece Mercer <reecemercer981@gmail.com>
2020-12-19 00:06:03 +0000

    Powershell port (#3)
    * File created
    
    * Updated readme and added PowerShell script
    
    * Made future cwebp version changes easier

---
+++ ConvertTo-Webp.ps1
@@ -0,0 +1,68 @@
+# Written by Reece Mercer
+
+<#
+    .SYNOPSIS
+    Takes a folder of images and converts them to WebP format
+    
+    .DESCRIPTION
+    Downloads libwebp, if required, and compresses images in a given folder
+    
+    .PARAMETER downloadLibWebp
+    Switch denoting whether libwebp is needed to be downloaded or not
+
+    .PARAMETER compressionFactor
+    The amount of compression to apply (default 80)
+
+    .PARAMETER directory
+    The path of the folder of images that is to be converted
+
+    .INPUTS
+    None.
+
+    .OUTPUTS
+    None
+
+    .EXAMPLE
+    ... you don't have libwebp installed
+    PS> ConvertTo-Webp -downloadLibWebp -directory ./images
+
+    .EXAMPLE
+    ... you do have libwebp installed
+    PS> ConvertTo-Webp -directory ./images
+    
+    .EXAMPLE
+    ... override default compression factor
+    PS> ConvertTo-Webp -directory ./images -compressionFactor 20
+#>
+
+param (
+    [switch]$downloadLibWebp,
+    [parameter(Mandatory = $false)]
+    [ValidateRange(1, 100)]
+    [int]$compressionFactor = 80,
+    [parameter(Mandatory = $true)]
+    [string]$directory
+)
+
+# What version of cwebp binaries do we want?
+$version = "1.1.0"
+$download = "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-$version-windows-x64.zip"
+
+if ($downloadLibWebp) {
+    Invoke-WebRequest $download -OutFile ./libwebp.zip; if ($?) { Expand-Archive ./libwebp.zip -DestinationPath ./libwebp -Force }
+    Remove-Item ./libwebp.zip
+    if (Test-Path $directory) {
+        Get-ChildItem $directory | ForEach-Object {
+            & ".\libwebp\libwebp-$version-windows-x64\bin\cwebp.exe" -q $compressionFactor $_.FullName -o "$($_.Name).webp"
+        }
+    }
+}
+else {
+    if (Test-Path $directory) {
+        Get-ChildItem $directory | ForEach-Object {
+            cwebp.exe -q $compressionFactor $_.FullName -o "$($_.Name).webp"
+        }
+    }
+}
+
+Remove-Item -Recurse ./libwebp -Force
